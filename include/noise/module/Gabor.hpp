576c00cb8fdd0d109fd34883589e600172de1def
Dalibor Frivaldsky <dalibor.free@gmail.com>
2013-02-10 20:30:54 +0100

    gabor - more granular selection of implementations base on instruction support level

--- include/noise/module/Gabor.hpp
+++ include/noise/module/Gabor.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+
 #include <noise/Platform.hpp>
+#include <noise/BasicTypes.hpp>
 
 
 
@@ -11,8 +13,15 @@ namespace noise
 	namespace module
 	{
 
-		template< typename ValueT, unsigned Dimension >
+		template< typename ValueType, uint8 Dimension >
 		class Gabor;
+
+
+		namespace gabor
+		{
+			template< typename ValueType, uint8 Dimension, uint8 VectorSize >
+			class GaborImpl;
+		}
 
 	}
 
@@ -20,6 +29,26 @@ namespace noise
 
 #if defined( LIBNOISE2_AT_LEAST_SSE2 )
 #include <noise/module/gabor/Gabor_SSE2.hpp>
+namespace noise
+{
+	namespace module
+	{
+		template< typename ValueType, uint8 Dimension >
+		class Gabor: public gabor::GaborImpl< ValueType, Dimension, 4 >
+		{
+		};
+	}
+}
 #else
 #include <noise/module/gabor/Gabor_No_SSE.hpp>
+namespace noise
+{
+	namespace module
+	{
+		template< typename ValueType, uint8 Dimension >
+		class Gabor: public gabor::GaborImpl< ValueType, Dimension, 1 >
+		{
+		};
+	}
+}
 #endif
